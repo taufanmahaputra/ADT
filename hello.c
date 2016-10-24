5ec8ed4ae82b039138cf14b4d5ae94c1b35d338d
taufanmahaputra <hacker.indo62@gmail.com>
2016-10-24 17:05:51 +0700

    Create hello.c

---
+++ hello.c
@@ -0,0 +1,15 @@
+/* NIM 			: 13515028
+Nama 			: Taufan Mahaputra
+Tanggal			: 25 Agustus 2016
+Topik praktikum	: Pengenalan bahasa C
+Deskripsi		: Program menampilkan sesuatu kelayar */
+
+#include <stdio.h>
+
+int main() {
+	
+	printf("Hello, World!\n");
+	
+	
+	return 0;
+}
